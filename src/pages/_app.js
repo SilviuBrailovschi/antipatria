11ec2b714b92d92d6cc57865c330030db7270fd5
SilviuBrailovschi <silviu.brailovschi@gmail.com>
2023-06-11 17:00:37 +0300

    chore: code cleanup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import MainLayout from "@/components/layout/main-layout";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import "../styles/globals.css";
 import "../styles/general.sass";
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
--- src/pages/contactUs/index.js
+++ src/pages/contactUs/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { CatEvent } from "../../components/events/catEvent";
 
 export default function ContactUs() {
   return <section>Contact Us</section>;
